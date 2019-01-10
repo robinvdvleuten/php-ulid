77d0eb274d933cebaa0ab2e2db26277e7741f3ca
Massimiliano Arione <garakkio@gmail.com>
2019-01-10 15:05:22 +0100

    exploit php 7.1 features (#7)

--- tests/UlidTest.php
+++ tests/UlidTest.php
@@ -14,38 +14,38 @@ namespace Ulid\Tests;
 use PHPUnit\Framework\TestCase;
 use Ulid\Ulid;
 
-class UlidTest extends TestCase
+final class UlidTest extends TestCase
 {
-    public function testGeneratesUppercaseIdentiferByDefault()
+    public function testGeneratesUppercaseIdentiferByDefault(): void
     {
         $this->assertRegExp('/[0-9][A-Z]/', (string) Ulid::generate());
     }
 
-    public function testGeneratesLowercaseIdentiferWhenConfigured()
+    public function testGeneratesLowercaseIdentiferWhenConfigured(): void
     {
         $this->assertRegExp('/[0-9][a-z]/', (string) Ulid::generate(true));
     }
 
-    public function testGeneratesTwentySixChars()
+    public function testGeneratesTwentySixChars(): void
     {
-        $this->assertSame(26, strlen(Ulid::generate()));
+        $this->assertSame(26, \strlen(Ulid::generate()));
     }
 
-    public function testGeneratesLexographicallySortableUlids()
+    public function testGeneratesLexographicallySortableUlids(): void
     {
         $a = Ulid::generate();
 
-        sleep(1);
+        \sleep(1);
 
         $b = Ulid::generate();
 
         $ulids = [(string) $b, (string) $a];
-        usort($ulids, 'strcmp');
+        \usort($ulids, 'strcmp');
 
         $this->assertSame([(string) $a, (string) $b], $ulids);
     }
 
-    public function testCreatesFromString()
+    public function testCreatesFromString(): void
     {
         $this->assertEquals('01AN4Z07BY79KA1307SR9X4MV3', (string) Ulid::fromString('01AN4Z07BY79KA1307SR9X4MV3'));
     }
